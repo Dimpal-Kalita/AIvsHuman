4d35ed7bb5710308c80e2f13a3ebcc1bf61ff5ec
Dimpal Kalita <kalitadimpal112@gmail.com>
2024-05-04 18:32:40 +0530

    CHG: Contributor Data

--- src/Pages/Contributors/Contributors.jsx
+++ src/Pages/Contributors/Contributors.jsx
@@ -1,12 +1,12 @@
 // Contributors.jsx
 import React, { useState, useEffect } from "react";
-import { FaEnvelope, FaPhone } from "react-icons/fa"; // Import icons from react-icons library
+import { FaEnvelope } from "react-icons/fa"; // Import icons from react-icons library
 import "./Contributors.scss";
 
 const Contributors = () => {
   const [professors, setProfessors] = useState([]);
   const [students, setStudents] = useState([]);
-
+  const [researchScholars, setResearchScholars] = useState([]);
   useEffect(() => {
     fetch("/db/contributors.json")
       .then((response) => {
@@ -18,8 +18,12 @@ const Contributors = () => {
       .then((data) => {
         const profs = data.filter((contributor) => contributor.role === "Professor");
         const stus = data.filter((contributor) => contributor.role === "Student");
+        const resScholars = data.filter(
+          (contributor) => contributor.role === "Research Scholar"
+        );
         setProfessors(profs);
         setStudents(stus);
+        setResearchScholars(resScholars);
       })
       .catch((error) => console.error("Error fetching contributors:", error));
   }, []);
@@ -42,10 +46,10 @@ const Contributors = () => {
               <FaEnvelope className="email-icon" />
               <span>{contributor.email}</span>
             </div>
-            <div className="contact-info">
+            {/* <div className="contact-info">
               <FaPhone className="phone-icon" />
               <span>{contributor.phone}</span>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
@@ -55,8 +59,12 @@ const Contributors = () => {
     <div className="contributors">
       <div className="contributors-container">
         <div className="contributors-section">
-          <h1 className="contributors-heading">Professors</h1>
+          <h1 className="contributors-heading">Guides</h1>
           <div className="contributors">{renderContributors(professors)}</div>
+        </div>
+        <div className="contributors-section">
+          <h1 className="contributors-heading">Research Scholars</h1>
+          <div className="contributors">{renderContributors(researchScholars)}</div>
         </div>
         <div className="contributors-section">
           <h1 className="contributors-heading">Students</h1>
