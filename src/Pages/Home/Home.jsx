ab3ab3d58d65655b921487041a7a0b4e9a70607c
Dimpal Kalita <kalitadimpal112@gmail.com>
2024-05-18 00:26:53 +0530

    ADD: read prediction from backend

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
         throw new Error("Text is not defined");
       }
 
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://aivshuman.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
